dc2192398bd26dd37579db0393c47e3ae2bbd582
Marcela Earley <Marcela.earley1024@gmail.com>
2019-10-05 13:10:16 -0700

    update local branch to master branch

--- models/schema.sql
+++ models/schema.sql
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 -- DROP DATABASE IF EXISTS plants_db;
 -- CREATE DATABASE plants_db;
 
@@ -22,3 +23,40 @@
 -- );
 
 -- INSERT INTO Plant(plantName) VALUES ('testing');
+=======
+DROP DATABASE IF EXISTS plants_db;
+CREATE DATABASE plants_db;
+
+DROP DATABASE IF EXISTS testdb;
+CREATE DATABASE testdb;
+
+
+CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
+
+CREATE TABLE TestTable AS
+SELECT customername, contactname
+FROM customers;
+
+CREATE TABLE Persons (
+    ID int NOT NULL,
+    LastName varchar(255) NOT NULL,
+    FirstName varchar(255),
+    Age int,
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE Persons (
+    ID int NOT NULL,
+    LastName varchar(255) NOT NULL,
+    FirstName varchar(255),
+    Age int,
+    CONSTRAINT PK_Person PRIMARY KEY (ID,LastName)
+);
+
+>>>>>>> Stashed changes
