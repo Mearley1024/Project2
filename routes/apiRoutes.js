060c68feb49ccf3a8d9328678299e576d916d35c
Marcela Earley <Marcela.earley1024@gmail.com>
2019-10-08 17:50:03 -0700

    update logo images

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,7 +9,7 @@ module.exports = function(app) {
   });
 
   // Update user with new plant
-
+  
   // Get all users
   app.get("/api/users", function(req, res) {
     db.Users.findAll({}).then(function(dbUser) {
